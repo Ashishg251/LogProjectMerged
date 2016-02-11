76e2407d4a8cfa4fc4fc4cd1317b4fdc3c395da6
tarun0409 <tarun.mohandas@gmail.com>
2016-02-11 17:32:08 +0530

    log rate data changed query

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
  var db1 = mongoose.createConnection("mongodb://172.23.238.253:27018/nginx");
- var db2 = mongoose.createConnection("mongodb://172.23.238.253:27018/LogAggregate");
+ var db2 = mongoose.createConnection("mongodb://172.23.238.253:27018/aptcache");
 //var db1 = mongoose.createConnection("mongodb://localhost/nginx");
 //var db2 = mongoose.createConnection("mongodb://localhost/LogAggregate");
 
@@ -8,12 +8,11 @@ var userSchema = require('./log.user.model');
 var serverSchema = require('./log.server.model');
 var configSchema = require('./log.config.model');
 var aptLogSchema = require('./logSchema');
-var aptConfigSchema = require('./configSchema');
+//var aptConfigSchema = require('./configSchema');
 
 module.exports = {
   userModel : db1.model('User',userSchema),
   serverModel : db1.model('Logs',serverSchema),
   configModel : db1.model('Config',configSchema),
-  aptLogModel : db2.model('aptLog',aptLogSchema),
-  aptConfigModel : db2.model('aptConfig', aptConfigSchema)
+  aptLogModel : db2.model('aptLog',aptLogSchema)
 };
--- routes/aptCache/logRateData.js
+++ routes/aptCache/logRateData.js
@@ -6,9 +6,24 @@ var Logs = require('../../models/dbConfig').aptLogModel;
 
 
 var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  timeObj = new Object();
+  timeObj["date"] = date;
+  timeObj["month"] = month;
+  timeObj["year"] = year;
+  timeObj["time"] = hour + ':' + min + ':' + sec;
+  return timeObj;
+}
 function createMonthlyData(result,year,month){
     data = new Array();
-    monthPos = months.indexOf(month);
+    var monthPos = months.indexOf(month);
     var days = new Date(year, monthPos+1, 0).getDate();
     for(var i=1; i<=days; i++){
         tempObj = new Object();
@@ -19,11 +34,12 @@ function createMonthlyData(result,year,month){
     }
     var LENGTH = result.length;
     for(var i=0; i<LENGTH; i++){
-          if(result[i]["_id"]["Type"]==="I"){
-            data[result[i]["_id"]["period"]-1]["Input"]+=result[i]["count"];
+          var timeObj = timeConverter(parseInt(result[i]["timestamp"]));
+          if(result[i]["mode"]==="I"){
+            data[timeObj["date"]-1]["Input"]+=1;
           }
           else{
-            data[result[i]["_id"]["period"]-1]["Output"]+=result[i]["count"];
+            data[timeObj["date"]-1]["Output"]+=1;
           }
     }
     return data;
@@ -39,68 +55,82 @@ function createYearlyData(result){
     }
     var LENGTH = result.length;
     for(var i=0; i<LENGTH; i++){
-          if(result[i]["_id"]["Type"]==="I"){
-            data[months.indexOf(result[i]["_id"]["period"])]["Input"]+=result[i]["count"];
+          var timeObj = timeConverter(parseInt(result[i]["timestamp"]));
+          //console.log(timeObj);
+          if(result[i]["mode"]==="I"){
+            data[months.indexOf(timeObj["month"])]["Input"]+=1;
           }
           else{
-            data[months.indexOf(result[i]["_id"]["period"])]["Output"]+=result[i]["count"];
+            data[months.indexOf(timeObj["month"])]["Output"]+=1;
           }
     }
+    console.log(data);
     return data;
 }
 function makeQuery(packageType,periodType,periodName){
-    var groupParams = new Array();
-    var match = new Object();
-    var group = new Object();
+    var queryParams = new Object()
     if(packageType==="all"){
         if(periodType==="yearly"){
-            match["year"]=parseInt(periodName);
-            group["period"] = "$month";
-            group["Type"] = "$mode";
+            var starttime = Date.parse("Jan 01, "+periodName+" 00:00:00")/1000;
+            var endtime = Date.parse("Dec 31, "+periodName+" 23:59:59")/1000;
+            queryParams["timestamp"] = new Object();
+            queryParams["timestamp"]["$gte"] = starttime;
+            queryParams["timestamp"]["$lte"] = endtime;
         }
         else if(periodType === "monthly"){
             periods = periodName.split("_");
-            match["year"]=parseInt(periods[0]);
-            match["month"]=periods[1];
-            group["period"] = "$date";
-            group["Type"] = "$mode";
+            var monthPos = months.indexOf(periods[1]);
+            var days = new Date(parseInt(periods[0]), monthPos+1, 0).getDate();
+            var starttime = Date.parse(periods[1]+" 01, "+periods[0]+" 00:00:00")/1000;
+            var endtime = Date.parse(periods[1]+" "+days+", "+periods[0]+" 23:59:59")/1000;
+            queryParams["timestamp"] = new Object();
+            queryParams["timestamp"]["$gte"] = starttime;
+            queryParams["timestamp"]["$lte"] = endtime;
         }
     }
     else if(packageType==="metadata"){
         if(periodType==="yearly"){
-            match["year"]=parseInt(periodName);
-            match["download"] = {$not:/.deb/};
-            group["period"] = "$month";
-            group["Type"] = "$mode";
+            queryParams["path"] = {$not:/.deb/};
+            var starttime = Date.parse("Jan 01, "+periodName+" 00:00:00")/1000;
+            var endtime = Date.parse("Dec 31, "+periodName+" 23:59:59")/1000;
+            queryParams["timestamp"] = new Object();
+            queryParams["timestamp"]["$gte"] = starttime;
+            queryParams["timestamp"]["$lte"] = endtime;
         }
         else if(periodType === "monthly"){
+            match["path"] = {$not:/.deb/};
             periods = periodName.split("_");
-            match["year"]=parseInt(periods[0]);
-            match["month"]=periods[1];
-            match["download"] = {$not:/.deb/};
-            group["period"] = "$date";
-            group["Type"] = "$mode";
+            var monthPos = months.indexOf(periods[1]);
+            var days = new Date(parseInt(periods[0]), monthPos+1, 0).getDate();
+            var starttime = Date.parse(periods[1]+" 01, "+periods[0]+" 00:00:00")/1000;
+            var endtime = Date.parse(periods[1]+" "+days+", "+periods[0]+" 23:59:59")/1000;
+            queryParams["timestamp"] = new Object();
+            queryParams["timestamp"]["$gte"] = starttime;
+            queryParams["timestamp"]["$lte"] = endtime;
         }
     }
     else if(packageType==="package"){
         if(periodType==="yearly"){
-            match["year"]=parseInt(periodName);
-            match["download"] = /.deb/;
-            group["period"] = "$month";
-            group["Type"] = "$mode";
+            queryParams["path"] = /.deb/;
+            var starttime = Date.parse("Jan 01, "+periodName+" 00:00:00")/1000;
+            var endtime = Date.parse("Dec 31, "+periodName+" 23:59:59")/1000;
+            queryParams["timestamp"] = new Object();
+            queryParams["timestamp"]["$gte"] = starttime;
+            queryParams["timestamp"]["$lte"] = endtime;
         }
         else if(periodType === "monthly"){
+            queryParams["path"] = /.deb/;
             periods = periodName.split("_");
-            match["year"]=parseInt(periods[0]);
-            match["month"]=periods[1];
-            match["download"] = /.deb/;
-            group["period"] = "$date";
-            group["Type"] = "$mode";
+            var monthPos = months.indexOf(periods[1]);
+            var days = new Date(parseInt(periods[0]), monthPos+1, 0).getDate();
+            var starttime = Date.parse(periods[1]+" 01, "+periods[0]+" 00:00:00")/1000;
+            var endtime = Date.parse(periods[1]+" "+days+", "+periods[0]+" 23:59:59")/1000;
+            queryParams["timestamp"] = new Object();
+            queryParams["timestamp"]["$gte"] = starttime;
+            queryParams["timestamp"]["$lte"] = endtime;
         }
     }
-    groupParams.push(match);
-    groupParams.push(group);
-    return groupParams;
+    return queryParams;
 }
 router.get('/:charttype=?/:urldata=?',function(req,res,next){
     var charttype = req.params.charttype;
@@ -120,9 +150,7 @@ router.get('/:charttype=?/:urldata=?',function(req,res,next){
         periodName = param[0]+"_"+param[1];
     }
     var queryParams = makeQuery(packageType,periodType,periodName);
-    Logs.aggregate([
-      {$match:queryParams[0]},
-      {$group:{_id: queryParams[1],count:{$sum:1}}}],
+    Logs.find(queryParams,
       function(err,result){
         var jsonData = undefined;
         if(periodType==="yearly"){
