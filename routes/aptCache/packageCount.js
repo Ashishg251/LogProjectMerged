9ba2d8bb5cb00d26cfcf4ac874431fac6b28630d
Bhairavi Balakrishnan <bhairavi.balakrishnan@wipro.com>
2016-02-11 17:03:57 +0530

    added licensing to our project

--- routes/aptCache/packageCount.js
+++ routes/aptCache/packageCount.js
@@ -1,3 +1,19 @@
+/*Copyright 2016 Wipro Limited, NIIT Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This code is written by Ashish Gupta, Tarun Mohandas, Suriya Prakash, Srinivasa Burli, Jishnu Surendran and Bhairavi Balakrishnan*/
+
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
@@ -49,7 +65,7 @@ router.get('/year/year_month/:year_month=?', function(req, res, next) {
       {
         finalresult.push(obj[item]);
       }
-      
+
       res.json(finalresult);
     }
   );
